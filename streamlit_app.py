dbb2c94e2e2f00314b2b65397c45c5b13239b822
Sujith Babu K R <sujith.babu@outlook.com>
2022-05-19 16:29:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner.')
 streamlit.header('Breakfast Menu')
-streamlit.text('oatmeal with blueberry;)
+streamlit.text('oatmeal with blueberry')
 streamlit.text('hard boiled free range egg')
