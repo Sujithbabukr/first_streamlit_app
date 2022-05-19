2cc8e45ac35b7f4b37c37a4e78fedd5ea3b4b087
Sujith Babu K R <sujith.babu@outlook.com>
2022-05-19 19:10:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,7 @@ import streamlit
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-                                         
-
-
+                                        
 streamlit.title('My Mom s healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
@@ -12,7 +10,7 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 #create multivalue picklist
-streamlit.multiselect("Pick Some Fruits:",list(my_fruit_list.index),['Aple','Orange'])
+streamlit.multiselect("Pick Some Fruits:",list(my_fruit_list.index),['Apple','Orange'])
 # display Dataframe
 streamlit.dataframe(my_fruit_list)
 
