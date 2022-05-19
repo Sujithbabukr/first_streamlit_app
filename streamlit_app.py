a22f3267a30554223baae907b1f12797545ec583
Sujith Babu K R <sujith.babu@outlook.com>
2022-05-19 17:24:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Mom'''s healthy Diner')
+streamlit.title('My Mom''''s healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
