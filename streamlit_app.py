cfe46bddc179ffc90883900cd117d52017dbee2d
Sujith Babu K R <sujith.babu@outlook.com>
2022-05-19 17:13:44 +0530

    Update streamlit_app.py
    Menu items Changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Dinner.')
-streamlit.header('Breakfast Menu')
-streamlit.text('oatmeal with blueberry')
-streamlit.text('hard boiled free range egg')
+streamlit.title(My Mom'S healthy Diner')
+streamlit.header('Breakfast Favorites')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
