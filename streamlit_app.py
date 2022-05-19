3759c56223b7f0ed2d4612814c887ced10532e66
Sujith Babu K R <sujith.babu@outlook.com>
2022-05-19 18:30:34 +0530

    Update streamlit_app.py
    add pandas and dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+
 streamlit.title('My Mom s healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
